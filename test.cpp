6a7669858f831c99018b78555ae24e5ddac95899
portiabh1 <60768057+portiabh1@users.noreply.github.com>
2022-02-07 11:24:10 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,3 +2,11 @@
 #include "catch.hpp"
 #include "functions_to_implement.cpp"
 #include <vector>
+
+TEST_CASE ( "Sign testing", "[sign]") {
+  REQUIRE( Sign(6) == 1 );
+  REQUIRE( Factorial(-1000) == -1 );
+  REQUIRE( Factorial(0) == 0 );
+  REQUIRE( Factorial(1234892379248) == 1 );
+  REQUIRE( Factorial(-2039873628719) == -1 );
+}
